fd2a1e068c511f02b6f2b5c647c4a4d09fee9767
Akshaya409 <akshayagarikapati05@gmail.com>
2025-12-14 14:04:51 +0530

    Add files via upload

---
+++ Sum,Difference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,diff;
+	printf("enter the value of a,b");
+	scanf("%d,%d",&a,&b);
+	sum=a+b;
+	printf("sum=%d\n",sum);
+	diff=a-b;
+	printf("diff=%d\n",diff);
+	return 0;
+	
+}
