6bd938389395b238094d5592cec754c3438e8035
Akshaya409 <akshayagarikapati05@gmail.com>
2025-12-14 14:00:49 +0530

    Add files via upload

---
+++ Data types.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	char d;
+	double c;
+	printf("enter the value os a");
+	scanf("%d",&a);
+	printf("enter the float value of b");
+	scanf("%f",&b);
+	printf("enter the character of d");
+	scanf("%c",&d);
+	printf("enter the double value of c");
+	scanf("lf",&c);
+	printf("Integer =%d\n",a);
+	printf("Float =%f\n",b);
+	printf("Character =%c\n",d);
+	printf("Double =%lf\n",c);
+	return 0;
+}
